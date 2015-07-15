a9e2e43d541fe22648541716024e1158e8d3f5ea
Radu <radu.bordei89@gmail.com>
2015-07-15 10:02:39 +0300

    adding service provider.

---
+++ src/ScaffoldServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Flysap\Scaffold;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScaffoldServiceProvider extends Serviceprovider {
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        // TODO: Implement register() method.
+    }
+}
