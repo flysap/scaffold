5cfc0c038578a720f12ae2f064667b0ea76bb625
Radu <radu.bordei89@gmail.com>
2015-07-21 16:53:11 +0300

    adding helpers functions.

---
+++ src/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Flysap\Scaffold;
+
+/**
+ * Change array keys recursive .
+ *
+ * @param $arr
+ * @return array
+ */
+function array_change_key_case_recursive($arr) {
+    return array_map(function ($item) {
+        if (is_array($item))
+            $item = array_change_key_case_recursive($item);
+        return $item;
+    }, array_change_key_case($arr));
+}
