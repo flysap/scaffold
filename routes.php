edec4c007e90a4a1929b2a16de7bb7a075b3c272
Radu <radu.bordei89@gmail.com>
2015-08-03 18:30:04 +0300

    adding routes scaffold.

---
+++ routes.php
@@ -0,0 +1,14 @@
+<?php
+
+Route::group(['prefix' => 'scaffold', 'as' => 'scaffold::'], function() {
+
+    Route::get('{eloquent_path}', ['as' => 'main', function($file) {
+        return app('scaffold')
+            ->lists($file);
+    }])->where('eloquent_path', "^([a-z\\/]+)");
+
+    Route::get('{eloquent_path}/{id}/edit', ['as' => 'edit', function($file, $id) {
+        return app('scaffold')
+            ->update($file, $id);
+    }])->where(['eloquent_path' => "^([a-z\\/]+)", 'id' => "(\\d+)"]);
+});
