5c9e685fc709860e45069205076e08f2ba3ea782
Radu <radu.bordei89@gmail.com>
2015-08-03 18:32:09 +0300

    improve scaffold service class.

--- src/ScaffoldService.php
+++ src/ScaffoldService.php
@@ -2,14 +2,78 @@
 
 namespace Flysap\Scaffold;
 
-use Flysap\Scaffold\Contracts\ScaffoldServiceContract;
+use Flysap\Support;
+use Modules;
 
-class ScaffoldService implements ScaffoldServiceContract {
+class ScaffoldService {
+
+    public function lists($model) {
+        $eloquent = $this->getModel($model);
+
+        $form     = $this->getBuilder()->fromEloquent($eloquent);
+
+        return view('scaffold::scaffold.lists', compact('form'));
+    }
 
     public function create() {
 
     }
 
-    public function update() {}
+    public function update($model, $id) {
+        $eloquent = $this->getModel($model, $id);
+
+        $form     = $this->getBuilder()->fromEloquent($eloquent);
+
+        return view('scaffold::scaffold.edit', compact('form'));
+    }
+
+    /**
+     * Get file instance ..
+     *
+     * @param $model
+     * @param null $identificator
+     * @return mixed
+     */
+    private function getModel($file, $identificator = null) {
+        $spaces = $this->getSpaces();
+        list($vendor, $user, $model) = explode('/', $file);
+
+        foreach ($spaces as $space) {
+            $full = $space . DIRECTORY_SEPARATOR . $vendor . DIRECTORY_SEPARATOR . $user . DIRECTORY_SEPARATOR . ucfirst($model) . '.php';
+
+            if( ! Support\is_path_exists(
+                app_path('../' . $full)
+            ) )
+                continue;
+
+            if( ! Support\has_extension($full, 'php'))
+                continue;
+
+            require_once(app_path('../' . $full));
+
+            #@todo check psr4 ..
+            $class = 'Modules\\' . $vendor . '\\' . $user . '\\' . ucfirst($model);
+
+            if(! is_null($identificator))
+                $model = $class::find($identificator);
+            else
+                $model = new $class;
+        }
+
+        return $model;
+    }
+
+    /**
+     * Get namespaces .
+     *
+     * @return mixed
+     */
+    private function getSpaces() {
+        return config('scaffold.model_namespaces');
+    }
+
+    private function getBuilder() {
+        return app('form-builder');
+    }
 
 }
