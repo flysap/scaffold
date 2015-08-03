3715c7c35baa6d40b4f5e78a11e399e2d5775f70
Radu <radu.bordei89@gmail.com>
2015-08-03 18:30:15 +0300

    adding views.

---
+++ views/scaffold/lists.blade.php
@@ -0,0 +1,4 @@
+{!!Flysap\FormBuilder\open_form($form)!!}
+{!!Flysap\FormBuilder\render($form)!!}
+{!!Flysap\FormBuilder\render_button(['value' => 'Submit'])!!}
+{!!Flysap\FormBuilder\close_form($form)!!}
