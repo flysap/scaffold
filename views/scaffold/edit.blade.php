48a69453007bcb0ca101a1f2d940ab8a1f94571f
Radu <radu.bordei89@gmail.com>
2015-08-17 15:29:30 +0300

    extending layout.

--- views/scaffold/edit.blade.php
+++ views/scaffold/edit.blade.php
@@ -1,4 +1,36 @@
-{!!Flysap\FormBuilder\open_form($form)!!}
-{!!Flysap\FormBuilder\render($form)!!}
-{!!Flysap\FormBuilder\render_button(['value' => 'Submit'])!!}
-{!!Flysap\FormBuilder\close_form($form)!!}
+@extends('themes::layouts.default')
+
+@section('content')
+    <section class="content-header">
+        <h1>
+            Page Header
+            <small>Optional description</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+            <li class="active">Here</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="row">
+            <div class="col-xs-12">
+
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">Data Table With Full Features</h3>
+                    </div><!-- /.box-header -->
+                    <div class="box-body">
+                        {!!Flysap\FormBuilder\open_form($form)!!}
+                        {!!Flysap\FormBuilder\render($form, false)!!}
+                        {!!Flysap\FormBuilder\render_button(['value' => 'Submit', 'type' => 'submit'])!!}
+                        {!!Flysap\FormBuilder\close_form($form)!!}
+                    </div><!-- /.box-body -->
+                </div><!-- /.box -->
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+
+    </section><!-- /.content -->
+@endsection
