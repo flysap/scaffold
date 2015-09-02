fc9f7367e901fcfcb8bbfef663c90a95660e56d6
Radu <radu.bordei89@gmail.com>
2015-09-02 17:58:13 +0300

    add custom route.

--- routes.php
+++ routes.php
@@ -1,6 +1,17 @@
 <?php
 
+use Illuminate\Http\Request;
+
 Route::group(['prefix' => 'scaffold', 'as' => 'scaffold::'], function() {
+
+    /**
+     * This is an custom url where you can send custom request for your eloquent models.
+     *
+     */
+    Route::post('custom', ['as' => 'custom', function(Request $request) {
+        return app('scaffold')
+            ->custom($request);
+    }]);
 
     Route::match(['post', 'get'],'{eloquent_path}', ['as' => 'main', function($file) {
         return app('scaffold')
