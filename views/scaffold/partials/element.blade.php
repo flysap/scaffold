da55c8d177e78a102227084759bc29190e64f210
Radu <radu.bordei89@gmail.com>
2015-09-11 19:19:22 +0300

    if is allowed than render.

--- views/scaffold/partials/element.blade.php
+++ views/scaffold/partials/element.blade.php
@@ -1,9 +1,11 @@
-@if(view()->exists('scaffold::scaffold.elements.' . $element->getAttribute('type')))
-    @include('scaffold::scaffold.elements.' . $element->getAttribute('type'))
-@else
-    <div class="form-group">
-        <?php $label = $element->getAttribute('label') ?: $element->getAttribute('name'); ?>
-        <label>{{ucfirst($label)}}</label>
-        {!!Flysap\FormBuilder\render_element($element, $form, ['class' => 'form-control'])!!}
-    </div>
+@if( $element->isAllowed() )
+    @if(view()->exists('scaffold::scaffold.elements.' . $element->getAttribute('type')))
+        @include('scaffold::scaffold.elements.' . $element->getAttribute('type'))
+    @else
+        <div class="form-group">
+            <?php $label = $element->getAttribute('label') ?: $element->getAttribute('name'); ?>
+            <label>{{ucfirst($label)}}</label>
+            {!!Flysap\FormBuilder\render_element($element, $form, ['class' => 'form-control'])!!}
+        </div>
+    @endif
 @endif
