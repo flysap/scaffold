8a3c4d137fddbdf6e1aef466dd1d083f7394cb9e
Radu <radu.bordei89@gmail.com>
2015-10-20 14:41:01 +0300

    fix.

--- routes.php
+++ routes.php
@@ -24,12 +24,12 @@ Route::group(['prefix' => 'admin/scaffold', 'as' => 'scaffold::', 'middleware' =
             ->create($file);
     }])->where('eloquent_path', "([a-z_\\/]+)");
 
-    Route::match(['post', 'get'], 'edit/{id}/{eloquent_path}/', ['as' => 'edit', function($id, $file) {
+    Route::match(['post', 'get'], 'edit/{id}/{eloquent_path}', ['as' => 'edit', function($id, $file) {
         return app('scaffold')
             ->update($file, $id);
     }])->where(['eloquent_path' => "^([a-z_\\/]+)", 'id' => "(\\d+)"]);
 
-    Route::get('delete/{id}{eloquent_path}/', ['as' => 'delete', function($id, $file) {
+    Route::get('delete/{id}/{eloquent_path}', ['as' => 'delete', function($id, $file) {
         return app('scaffold')
             ->delete($file, $id);
     }])->where(['eloquent_path' => "^([a-z_\\/]+)", 'id' => "(\\d+)"]);
