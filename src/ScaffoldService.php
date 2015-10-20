c5b3b07dc8987b7cfe1f4a5662ca79078fa2c460
Radu <radu.bordei89@gmail.com>
2015-10-20 15:04:55 +0300

    remove func.

--- src/ScaffoldService.php
+++ src/ScaffoldService.php
@@ -347,40 +347,6 @@ DOC;
     }
 
     /**
-     * Get file instance ..
-     *
-     * @param $file
-     * @param null $identificator
-     * @return mixed
-     */
-    private function getModel($file, $identificator = null) {
-        $spaces = config('scaffold.model_namespaces');
-
-        list($vendor, $user, $model) = explode('/', $file);
-
-        foreach ($spaces as $space) {
-            $full = $space . DIRECTORY_SEPARATOR . $vendor . DIRECTORY_SEPARATOR . $user . DIRECTORY_SEPARATOR . ucfirst($model) . '.php';
-
-            if( ! Support\is_path_exists(
-                app_path('../' . $full)
-            ) )
-                continue;
-
-            if( ! Support\has_extension($full, 'php'))
-                continue;
-
-            require_once(app_path('../' . $full));
-
-            #@todo check psr4 ..
-            $class = 'Modules\\' . $vendor . '\\' . $user . '\\' . ucfirst($model);
-
-            $model = $class::findOrNew($identificator);
-        }
-
-        return $model;
-    }
-
-    /**
      * Check if is enabled smart search .
      *
      * @return bool
